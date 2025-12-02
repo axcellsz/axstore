91bae89b84215549ebc7e16078ac45c2385cabda
axcellsz <122135545+axcellsz@users.noreply.github.com>
2025-12-02 23:08:35 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,9 +1,5 @@
 export default {
   async fetch(request, env, ctx) {
-    return new Response("Hallo Indonesia!", {
-      headers: {
-        "content-type": "text/plain; charset=UTF-8"
-      }
-    });
+    return env.ASSETS.fetch(request);
   }
 };
